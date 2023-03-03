994178ff81d8bbe6923afd4594348e3270437623
aliciayerinkim <104707030+aliciayerinkim@users.noreply.github.com>
2023-03-03 17:44:51 -0600

    Update routes.js

--- backend/src/apis/routes.js
+++ backend/src/apis/routes.js
@@ -1,12 +1,12 @@
-/**
-* This file will contain a route that will be used to access Google Translate API for translation
-* @author Aaron Su - 930006201
-*/
-const { Router } = require('express');
-const controller = require('./controller');
+// /**
+// * This file will contain a route that will be used to access Google Translate API for translation
+// * @author Aaron Su - 930006201
+// */
+// const { Router } = require('express');
+// const controller = require('./controller');
 
-const router = Router();
+// const router = Router();
 
-router.post("/translate", controller.translateText);
+// router.post("/translate", controller.translateText);
 
-module.exports = router;
+// module.exports = router;
